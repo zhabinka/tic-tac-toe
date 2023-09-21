6807db5a0631b6698fba7658fd7ea927d9853a2c
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-09-21 21:23:08 +0300

    start battle with battle_manager

--- lib/battle_field.ex
+++ lib/battle_field.ex
@@ -61,6 +61,10 @@ defmodule TicTacToe.Game do
       GenServer.start_link(__MODULE__, :no_args, name: __MODULE__)
     end
 
+    def start_battle(battle_id) do
+      GenServer.call(__MODULE__, {:start_room, battle_id})
+    end
+
     @impl true
     def init(_) do
       state = %{
@@ -69,6 +73,20 @@ defmodule TicTacToe.Game do
 
       Logger.info("BattleManager has started with state #{inspect(state)}")
       {:ok, state}
+    end
+
+    @impl true
+    def handle_call({:start_room, battle_id}, _from, %{battles: battles} = state) do
+      {:ok, _} = Sup.start_battle(battle_id)
+      state = %{state | battles: [battle_id | battles]}
+      Logger.info("BattleManager has started battle #{battle_id}, state #{inspect(state)}")
+      {:reply, :ok, state}
+    end
+
+    # Catch all
+    def handle_call(message, _from, state) do
+      Logger.warning("BattleManager unknown call #{inspect(message)}")
+      {:noreply, state}
     end
   end
 end

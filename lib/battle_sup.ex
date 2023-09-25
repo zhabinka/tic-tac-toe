e7065e10e3ad9641e5139c4d5ce55950273d2f44
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-09-25 16:37:30 +0300

    remove battle registry

--- lib/battle_sup.ex
+++ lib/battle_sup.ex
@@ -3,18 +3,26 @@ defmodule TicTacToe.BattleSup do
   use DynamicSupervisor
 
   @sup_name :battle_sup
-  @registry_name :battle_registry
+  # @registry_name :battle_registry
 
   def start_link(_) do
-    Registry.start_link(keys: :unique, name: @registry_name)
+    # Registry.start_link(keys: :unique, name: @registry_name)
     DynamicSupervisor.start_link(__MODULE__, :no_args, name: @sup_name)
   end
 
-  def start_battle(battle_id) do
-    process_name = {:via, Registry, {@registry_name, battle_id}}
-    child_spec = {TicTacToe.Battle, {battle_id, process_name}}
+  def start_battle() do
+    # process_name = {:via, Registry, {@registry_name, battle_id}}
+    child_spec = {TicTacToe.Battle, :no_args}
     DynamicSupervisor.start_child(@sup_name, child_spec)
   end
+
+  # NOTE: Как сделать общее имя хранилища для разных модулей?
+  # def find_battle(battle_id) do
+  #   case Registry.lookup(:battle_registry, battle_id) do
+  #     [{pid, _}] -> {:ok, pid}
+  #     [] -> {:error, :not_found}
+  #   end
+  # end
 
   @impl true
   def init(_) do
